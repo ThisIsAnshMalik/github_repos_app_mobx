cd5282892bb2554339247dc16a8c2c60c0cf2e09
Ansh Malik <anshmalik279@gmail.com>
2022-09-15 19:05:39 +0530

    project completed

---
+++ lib/github_store.dart
@@ -0,0 +1,47 @@
+import 'package:github/github.dart';
+import 'package:mobx/mobx.dart';
+
+part 'github_store.g.dart';
+
+// ignore: library_private_types_in_public_api
+class GithubStore = _GithubStore with _$GithubStore;
+
+abstract class _GithubStore with Store {
+  final GitHub client = GitHub();
+  List<Repository> repositories = [];
+  @observable
+  ObservableFuture<List<Repository>> fetchReposFuture = emptyResponse;
+
+  @observable
+  String user = '';
+
+  @observable
+  bool loading = false;
+  @computed
+  bool get hasResults =>
+      fetchReposFuture != emptyResponse &&
+      fetchReposFuture.status == FutureStatus.fulfilled;
+
+  static ObservableFuture<List<Repository>> emptyResponse =
+      ObservableFuture.value([]);
+
+  @action
+  Future<List<Repository>> fetchRepos() async {
+    repositories = [];
+    final future = client.repositories.listUserRepositories(user).toList();
+    fetchReposFuture = ObservableFuture(future);
+
+    return repositories = await future;
+  }
+
+  @action
+  void setLoading(bool value) {
+    loading = value;
+  }
+
+  @action
+  void setUser(String text) {
+    fetchReposFuture = emptyResponse;
+    user = text;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:github_repos_app_mobx/github_store.dart';
+import 'package:mobx/mobx.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,12 +32,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GithubStore store = GithubStore();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              autocorrect: false,
+              onSubmitted: (String user) {
+                store.setUser(user);
+                store.fetchRepos();
+              },
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  labelText: "search",
+                  border: OutlineInputBorder()),
+            ),
+          ),
+          Observer(
+              builder: (_) =>
+                  store.fetchReposFuture.status == FutureStatus.pending
+                      ? const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: LinearProgressIndicator(),
+                        )
+                      : Container()),
+          Expanded(
+            child: Observer(builder: (_) {
+              if (!store.hasResults) {
+                return Container();
+              } else {
+                return ListView.builder(
+                    itemCount: store.repositories.length,
+                    itemBuilder: (_, int index) {
+                      final repo = store.repositories[index];
+                      return ListTile(
+                        title: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                repo.name,
+                                overflow: TextOverflow.fade,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Text(' (${repo.stargazersCount} ⭐️)'),
+                          ],
+                        ),
+                        subtitle: Text(
+                          repo.description,
+                          overflow: TextOverflow.fade,
+                        ),
+                      );
+                    });
+              }
+            }),
+          ),
+        ],
       ),
     );
   }
